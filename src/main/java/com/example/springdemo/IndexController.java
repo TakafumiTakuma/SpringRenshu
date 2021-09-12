7cfe285e47bb4ba47c69253aeb5415bef66b6513
TakafumiTakuma <flowesbelieve2136@gmail.com>
2021-09-12 23:50:42 +0900

    テンプレート機能確認

--- src/main/java/com/example/springdemo/IndexController.java
+++ src/main/java/com/example/springdemo/IndexController.java
@@ -1,59 +1,18 @@
 package com.example.springdemo;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-
+@Controller
 public class IndexController {
 
-    String[]  names = {"tetuya","hanako","taro"};
-    String[] mails = {"tetuya@aaa.com","hanako@bbb.com","tero@ccc.com"};
+    @RequestMapping("/")
 
-    @RequestMapping("/{id}")
-
-    public DataObject index(@PathVariable int id) {
-        return new DataObject(id, names[id], mails[id]);
+    public String index() {
+        return "index";
     }
 
 }
 
-class DataObject {
-    private int id;
-    private String name;
-    private String value;
-
-    public DataObject(int id, String name, String value ){
-        super();
-        this.id = id;
-        this.name = name;
-        this.value = value;
-
-    }
-
-    public int getId(){
-        return id;
-    }
-
-    public void setId(int id){
-        this.id = id;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    public void setName(String name){
-        this.name = name;
-    }
-
-    public String getValue(){
-        return value;
-    }
-
-    public void setValue(String value){
-        this.value = value;
-    }
-
-}
