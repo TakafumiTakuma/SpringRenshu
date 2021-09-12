f37203831a284d8a281109bd33e1b69e130ee2e3
TakafumiTakuma <flowesbelieve2136@gmail.com>
2021-09-12 19:52:25 +0900

    DataObject出力

--- src/main/java/com/example/springdemo/IndexController.java
+++ src/main/java/com/example/springdemo/IndexController.java
@@ -2,22 +2,58 @@ package com.example.springdemo;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class IndexController {
-    @RequestMapping(value = "/{num}", method = RequestMethod.GET)
 
-    public String index(@PathVariable int num){
-       int res = 0;
+    String[]  names = {"tetuya","hanako","taro"};
+    String[] mails = {"tetuya@aaa.com","hanako@bbb.com","tero@ccc.com"};
 
-       for(int i = 1;i <= num; i++)
-           res += i;
+    @RequestMapping("/{id}")
 
-           return "total;" + res;
+    public DataObject index(@PathVariable int id) {
+        return new DataObject(id, names[id], mails[id]);
+    }
 
+}
+
+class DataObject {
+    private int id;
+    private String name;
+    private String value;
+
+    public DataObject(int id, String name, String value ){
+        super();
+        this.id = id;
+        this.name = name;
+        this.value = value;
+
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getValue(){
+        return value;
+    }
+
+    public void setValue(String value){
+        this.value = value;
     }
 
 }
